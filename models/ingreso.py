deb8d48f329f494204927f9a183187aaf7b65f68
juandardi <40305927+juandardi@users.noreply.github.com>
2020-12-11 12:09:00 -0500

    Procfile fixed

--- models/ingreso.py
+++ models/ingreso.py
@@ -9,20 +9,3 @@ class IngresoOut(BaseModel):
     id_ingreso: int
     tipo: str
     valor: float
-
-'''
-class Salario(BaseModel):
-    valor: int
-    tipo = 'salario'
-    constante = True
-
-class GananciaOcasional(BaseModel):
-    valor: int
-    tipo = 'ocasional'  
-    constante = False
-
-class Inversion(BaseModel):
-    valor: int
-    tipo = 'inversion'
-    constante = False
-'''
